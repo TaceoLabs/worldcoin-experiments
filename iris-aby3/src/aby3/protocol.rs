da57740f1d0ac3ab1a251e8a18d211b615f18e5d
Roman Walch <walch@taceo.io>
2023-11-30 16:02:03 +0100

    .

--- iris-aby3/src/aby3/protocol.rs
+++ iris-aby3/src/aby3/protocol.rs
@@ -85,6 +85,25 @@ impl<N: NetworkTrait> Aby3<N> {
         (x1, x2, x3)
     }
 
+    fn pack_u128(&self, a: Vec<Share<Bit>>) -> Vec<Share<u128>> {
+        let outlen = (a.len() + 127) / 128;
+        let mut out = Vec::with_capacity(outlen);
+
+        for a_ in a.chunks(128) {
+            let mut share_a = 0u128;
+            let mut share_b = 0u128;
+            for (i, bit) in a_.iter().enumerate() {
+                let (bit_a, bit_b) = bit.get_ab();
+                share_a |= (bit_a.convert().convert() as u128) << i;
+                share_b |= (bit_b.convert().convert() as u128) << i;
+            }
+            let share = Share::new(RingElement(share_a), RingElement(share_b));
+            out.push(share);
+        }
+
+        out
+    }
+
     async fn reduce_or_u128(&mut self, a: Share<u128>) -> Result<Share<Bit>, Error> {
         let (a, b) = a.get_ab();
 
@@ -171,6 +190,33 @@ impl<N: NetworkTrait> Aby3<N> {
         }
 
         Ok(decomp[0].to_owned())
+    }
+
+    pub(crate) async fn or_tree_u128(
+        &mut self,
+        mut inputs: Vec<Share<u128>>,
+    ) -> Result<Share<u128>, Error> {
+        let mut num = inputs.len();
+
+        while num > 1 {
+            let mod_ = num & 1;
+            num >>= 1;
+
+            let a_vec = inputs.drain(..num).collect();
+            let b_vec = inputs.drain(..num).collect();
+
+            let mut res = self.or_many(a_vec, b_vec).await?;
+            debug_assert!(inputs.is_empty() || inputs.len() == 1);
+            for leftover in inputs {
+                res.push(leftover);
+            }
+            inputs = res;
+
+            num += mod_;
+        }
+
+        let output = inputs[0].to_owned();
+        Ok(output)
     }
 }
 
@@ -390,8 +436,9 @@ where
     }
 
     async fn reduce_binary_or(&mut self, a: Vec<Share<Bit>>) -> Result<Share<Bit>, Error> {
-        // TODO pack the results into one share
-        todo!()
+        let packed = self.pack_u128(a);
+        let reduced = self.or_tree_u128(packed).await?;
+        self.reduce_or_u128(reduced).await
     }
 }
 
