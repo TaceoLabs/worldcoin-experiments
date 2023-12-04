91fa4b06187d280c1a153ae9b656bce2539a3c9e
Daniel Kales <11509575+dkales@users.noreply.github.com>
2023-12-04 10:29:20 +0100

    refactor: minor changes to example script

--- iris-aby3/examples/run_aby3.sh
+++ iris-aby3/examples/run_aby3.sh
@@ -15,7 +15,7 @@ if [[ $# -eq 1 ]]; then
 fi
 
 mkdir -p data
-[[ -f data/db0.sqlite ]] || cargo run --bin create_sample_data -- -d data/db0.sqlite -i 1000
+[[ -f data/db0.sqlite ]] || cargo run --release --bin create_sample_data -- -d data/db0.sqlite -i 10000
 
 [[ -f "data/key0.der" ]] || cargo run --bin gen_cert -- -k data/key0.der -c data/cert0.der -s localhost -s party0
 [[ -f "data/key1.der" ]] || cargo run --bin gen_cert -- -k data/key1.der -c data/cert1.der -s localhost -s party1
@@ -24,3 +24,4 @@ mkdir -p data
 cargo run --release --example aby3 -- -p 0 -k data/key0.der -c examples/config.yaml -d data/db0.sqlite -i 0 $args &
 cargo run --release --example aby3 -- -p 1 -k data/key1.der -c examples/config.yaml -d data/db0.sqlite -i 0 $args &
 cargo run --release --example aby3 -- -p 2 -k data/key2.der -c examples/config.yaml -d data/db0.sqlite -i 0 $args
+#CARGO_PROFILE_RELEASE_DEBUG=true cargo flamegraph --example aby3 -- -p 2 -k data/key2.der -c examples/config.yaml -d data/db0.sqlite -i 0 $args
