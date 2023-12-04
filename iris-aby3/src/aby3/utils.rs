0dc68e56032fbcdb28208d86080087a0f30df6b9
Roman Walch <walch@taceo.io>
2023-12-04 15:30:17 +0100

    .g

--- iris-aby3/src/aby3/utils.rs
+++ iris-aby3/src/aby3/utils.rs
@@ -1,9 +1,5 @@
 use super::random::prf::PrfSeed;
-use crate::{
-    error::Error,
-    traits::network_trait::NetworkTrait,
-    types::ring_element::{ring_vec_from_bytes, ring_vec_to_bytes, RingImpl},
-};
+use crate::{error::Error, traits::network_trait::NetworkTrait};
 use bytes::{Buf, Bytes, BytesMut};
 use std::io::Error as IOError;
 
