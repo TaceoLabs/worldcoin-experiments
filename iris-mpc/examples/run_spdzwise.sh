b26355639aa2662f8586e7caf30fbebab9786e59
Roman Walch <walch@taceo.io>
2023-12-22 13:53:26 +0100

    create spdzwise db

---
+++ iris-mpc/examples/run_spdzwise.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ $# -gt 1 ]]; then
+    echo "Usage: run_spdzwise.sh [-s]"
+    exit -1
+fi
+
+args=""
+if [[ $# -eq 1 ]]; then
+    if [[ $1 != "-s" ]]; then
+        echo "Usage: run_spdzwise.sh [-s]"
+        exit -1
+    fi
+    args="-s"
+fi
+
+mkdir -p data
+[[ -f data/db2.sqlite ]] || cargo run --release --bin create_sample_data -- -d data/db2.sqlite -i 10000 -m spdzwise
+
+[[ -f "data/key0.der" ]] || cargo run --bin gen_cert -- -k data/key0.der -c data/cert0.der -s localhost -s party0
+[[ -f "data/key1.der" ]] || cargo run --bin gen_cert -- -k data/key1.der -c data/cert1.der -s localhost -s party1
+[[ -f "data/key2.der" ]] || cargo run --bin gen_cert -- -k data/key2.der -c data/cert2.der -s localhost -s party2
+
+cargo run --release --example spdzwise -- -p 0 -k data/key0.der -c examples/config.yaml -d data/db2.sqlite -i 0 $args &
+cargo run --release --example spdzwise -- -p 1 -k data/key1.der -c examples/config.yaml -d data/db2.sqlite -i 0 $args &
+cargo run --release --example spdzwise -- -p 2 -k data/key2.der -c examples/config.yaml -d data/db2.sqlite -i 0 $args
+#CARGO_PROFILE_RELEASE_DEBUG=true cargo flamegraph --example spdzwise -- -p 2 -k data/key2.der -c examples/config.yaml -d data/db2.sqlite -i 0 $args
--- iris-mpc/src/bin/create_sample_data.rs
+++ iris-mpc/src/bin/create_sample_data.rs
@@ -2,9 +2,9 @@ use std::path::PathBuf;
 
 use clap::Parser;
 use color_eyre::eyre::{Report, Result};
-use iris_mpc::prelude::{Aby3, Aby3Network, MpcTrait, Swift3, Swift3Network};
+use iris_mpc::prelude::{Aby3, Aby3Network, MpcTrait, SpdzWise, Swift3, Swift3Network};
 use plain_reference::IrisCode;
-use rand::{rngs::SmallRng, SeedableRng};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
 use rusqlite::Connection;
 
 #[derive(Parser)]
@@ -158,6 +158,118 @@ fn create_swift3_db(opts: Opts) -> Result<()> {
     Ok(())
 }
 
+fn create_spdzwise_db(opts: Opts) -> Result<()> {
+    let database_file = opts.database;
+    let num_items = opts.items;
+
+    let mut conn = open_database(&database_file)?;
+
+    // Create the table if it doesn't exist
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS iris_codes (
+            id INTEGER PRIMARY KEY,
+            code BLOB NOT NULL,
+            mask BLOB NOT NULL,
+            share_a BLOB NOT NULL,
+            mac_a BLOB NOT NULL,
+            share_b BLOB NOT NULL,
+            mac_b BLOB NOT NULL,
+            share_c BLOB NOT NULL,
+            mac_c BLOB NOT NULL
+        )",
+        rusqlite::params![],
+    )?;
+
+    // Create the table for the mac key if it doesn't exist
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS mac_key (
+                id INTEGER PRIMARY KEY,
+                key BLOB NOT NULL,
+                share_a BLOB NOT NULL,
+                share_b BLOB NOT NULL,
+                share_c BLOB NOT NULL
+            )",
+        rusqlite::params![],
+    )?;
+
+    let mut rng = SmallRng::from_entropy();
+    let mac_key = rng.gen::<u64>();
+
+    let mut codes = Vec::with_capacity(num_items as usize);
+    for _ in 0..num_items {
+        let code = IrisCode::random_rng(&mut rng);
+        codes.push(code);
+    }
+    // Insert the mac key into the database
+    let transaction = conn.transaction()?;
+    let mut stmt = transaction
+        .prepare("INSERT INTO mac_key (key, share_a, share_b, share_c) VALUES (?1, ?2, ?3, ?4)")?;
+
+    let shares = Aby3::<Aby3Network>::share(mac_key, 0, &mut rng);
+
+    let data_a = bincode::serialize(&shares[0].to_owned().get_a())?;
+    let data_b = bincode::serialize(&shares[1].to_owned().get_a())?;
+    let data_c = bincode::serialize(&shares[2].to_owned().get_a())?;
+
+    stmt.execute([mac_key.to_le_bytes().as_ref(), &data_a, &data_b, &data_c])?;
+
+    drop(stmt);
+    transaction.commit()?;
+
+    // Insert the codes into the database
+    let transaction = conn.transaction()?;
+    let mut stmt = transaction.prepare("INSERT INTO iris_codes (code, mask, share_a, mac_a, share_b, mac_b, share_c, mac_c) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)")?;
+
+    for code in codes {
+        let mut shared_code_a = Vec::with_capacity(IrisCode::IRIS_CODE_SIZE);
+        let mut shared_code_b = Vec::with_capacity(IrisCode::IRIS_CODE_SIZE);
+        let mut shared_code_c = Vec::with_capacity(IrisCode::IRIS_CODE_SIZE);
+        let mut mac_a = Vec::with_capacity(IrisCode::IRIS_CODE_SIZE);
+        let mut mac_b = Vec::with_capacity(IrisCode::IRIS_CODE_SIZE);
+        let mut mac_c = Vec::with_capacity(IrisCode::IRIS_CODE_SIZE);
+        for i in 0..IrisCode::IRIS_CODE_SIZE {
+            // We simulate the parties already knowing the shares of the code.
+            let shares = SpdzWise::<Aby3Network, _>::share(
+                u16::from(code.code.get_bit(i)),
+                mac_key,
+                &mut rng,
+            );
+            let (v1, m1) = shares[0].to_owned().get();
+            let (v2, m2) = shares[0].to_owned().get();
+            let (v3, m3) = shares[0].to_owned().get();
+
+            shared_code_a.push(v1.get_a());
+            shared_code_b.push(v2.get_a());
+            shared_code_c.push(v3.get_a());
+            mac_a.push(m1.get_a());
+            mac_b.push(m2.get_a());
+            mac_c.push(m3.get_a());
+        }
+
+        let data_a = bincode::serialize(&shared_code_a)?;
+        let data_b = bincode::serialize(&shared_code_b)?;
+        let data_c = bincode::serialize(&shared_code_c)?;
+        let data_mac_a = bincode::serialize(&mac_a)?;
+        let data_mac_b = bincode::serialize(&mac_b)?;
+        let data_mac_c = bincode::serialize(&mac_c)?;
+
+        stmt.execute([
+            code.code.as_raw_slice(),
+            code.mask.as_raw_slice(),
+            &data_a,
+            &data_mac_a,
+            &data_b,
+            &data_mac_b,
+            &data_c,
+            &data_mac_c,
+        ])?;
+    }
+    drop(stmt);
+    transaction.commit()?;
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
     let prot = opts.mpc.to_lowercase();
@@ -166,6 +278,8 @@ fn main() -> Result<()> {
         create_aby3_db(opts)
     } else if prot == "swift3" {
         create_swift3_db(opts)
+    } else if prot == "spdzwise" {
+        create_spdzwise_db(opts)
     } else {
         Err(Report::msg("Invalid MPC protocol specified"))
     }
