6fa666afdf17352f5272c6310c497d3cac85b637
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-01-02 13:18:05 +0100

    feat: implement a managed channel handle to do networking stuff in other tasks

--- mpc-net/src/channel.rs
+++ mpc-net/src/channel.rs
@@ -1,9 +1,13 @@
 use futures::{Sink, SinkExt, Stream, StreamExt};
 use std::{
     io::{self},
+    marker::Unpin,
     pin::Pin,
 };
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    sync::{mpsc, oneshot},
+};
 use tokio_util::codec::{Decoder, Encoder, FramedRead, FramedWrite, LengthDelimitedCodec};
 
 pub type ReadChannel<T, D> = FramedRead<T, D>;
@@ -17,14 +21,14 @@ pub struct Channel<R, W, C> {
 
 pub type BytesChannel<R, W> = Channel<R, W, LengthDelimitedCodec>;
 
-impl<R: AsyncReadExt + std::marker::Unpin, W: AsyncWriteExt + std::marker::Unpin, C>
-    Channel<R, W, C>
-{
+impl<R, W, C> Channel<R, W, C> {
     /// Create a new [`Channel`], backed by a read and write half. Read and write buffers
     /// are automatically handled by [`LengthDelimitedCodec`].
     pub fn new<MSend>(read_half: R, write_half: W, codec: C) -> Self
     where
         C: Clone + Decoder + Encoder<MSend>,
+        R: AsyncReadExt,
+        W: AsyncWriteExt,
     {
         Channel {
             write_conn: FramedWrite::new(write_half, codec.clone()),
@@ -39,6 +43,8 @@ impl<R: AsyncReadExt + std::marker::Unpin, W: AsyncWriteExt + std::marker::Unpin
     pub async fn close<MSend>(self) -> Result<(), io::Error>
     where
         C: Encoder<MSend, Error = std::io::Error> + Decoder<Error = std::io::Error>,
+        R: AsyncReadExt + Unpin,
+        W: AsyncWriteExt + Unpin,
     {
         let Channel {
             mut read_conn,
@@ -64,10 +70,10 @@ impl<R: AsyncReadExt + std::marker::Unpin, W: AsyncWriteExt + std::marker::Unpin
         Ok(())
     }
 }
-impl<R, W: AsyncWriteExt + std::marker::Unpin, MSend, C: Encoder<MSend, Error = io::Error>>
-    Sink<MSend> for Channel<R, W, C>
+impl<R, W: AsyncWriteExt + Unpin, MSend, C: Encoder<MSend, Error = io::Error>> Sink<MSend>
+    for Channel<R, W, C>
 where
-    Self: std::marker::Unpin,
+    Self: Unpin,
 {
     type Error = <C as Encoder<MSend>>::Error;
 
@@ -96,14 +102,10 @@ where
         self.write_conn.poll_close_unpin(cx)
     }
 }
-impl<
-        R: AsyncReadExt + std::marker::Unpin,
-        W,
-        MRecv,
-        C: Decoder<Item = MRecv, Error = io::Error>,
-    > Stream for Channel<R, W, C>
+impl<R: AsyncReadExt + Unpin, W, MRecv, C: Decoder<Item = MRecv, Error = io::Error>> Stream
+    for Channel<R, W, C>
 where
-    Self: std::marker::Unpin,
+    Self: Unpin,
 {
     type Item = Result<MRecv, <C as Decoder>::Error>;
 
@@ -112,5 +114,88 @@ where
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
         self.read_conn.poll_next_unpin(cx)
+    }
+}
+
+struct WriteJob<MSend> {
+    data: MSend,
+    ret: oneshot::Sender<Result<(), io::Error>>,
+}
+
+struct ReadJob<MRecv> {
+    ret: oneshot::Sender<Result<MRecv, io::Error>>,
+}
+
+pub struct ChannelHandle<MSend, MRecv> {
+    write_job_queue: mpsc::Sender<WriteJob<MSend>>,
+    read_job_queue: mpsc::Sender<ReadJob<MRecv>>,
+}
+
+impl<MSend, MRecv> ChannelHandle<MSend, MRecv>
+where
+    MRecv: Send + 'static,
+    MSend: Send + 'static,
+{
+    pub fn manage<R, W, C>(chan: Channel<R, W, C>) -> ChannelHandle<MSend, MRecv>
+    where
+        C: 'static,
+        R: AsyncReadExt + Unpin + 'static,
+        W: AsyncWriteExt + Unpin + 'static,
+        FramedRead<R, C>: Stream<Item = Result<MRecv, io::Error>> + Send,
+        FramedWrite<W, C>: Sink<MSend, Error = io::Error> + Send,
+    {
+        let (write_send, mut write_recv) = mpsc::channel::<WriteJob<MSend>>(8);
+        let (read_send, mut read_recv) = mpsc::channel::<ReadJob<MRecv>>(8);
+
+        let (mut write, mut read) = chan.split();
+
+        tokio::spawn(async move {
+            while let Some(read_job) = read_recv.recv().await {
+                let read_result = match read.next().await {
+                    None => Err(io::Error::new(io::ErrorKind::BrokenPipe, "closed pipe")),
+                    Some(res) => res,
+                };
+
+                // we don't really care if the receiver is gone, although most of the time this would be a usage error, so at least emit a warning
+                match read_job.ret.send(read_result) {
+                    Ok(_) => {}
+                    Err(_) => {
+                        tracing::warn!("Warning: Read Job finished but receiver is gone!");
+                    }
+                }
+            }
+        });
+        tokio::spawn(async move {
+            while let Some(write_job) = write_recv.recv().await {
+                let write_result = write.send(write_job.data).await;
+                // we don't really care if the receiver for a write job is gone, as this is a common case
+                // therefore we only emit a trace message
+                match write_job.ret.send(write_result) {
+                    Ok(_) => {}
+                    Err(_) => {
+                        tracing::trace!("Debug: Write Job finished but receiver is gone!");
+                    }
+                }
+            }
+        });
+
+        ChannelHandle {
+            write_job_queue: write_send,
+            read_job_queue: read_send,
+        }
+    }
+
+    pub fn send(&mut self, data: MSend) -> oneshot::Receiver<Result<(), io::Error>> {
+        let (ret, recv) = oneshot::channel();
+        let job = WriteJob { data, ret };
+        self.write_job_queue.try_send(job).unwrap();
+        recv
+    }
+
+    pub fn recv(&mut self) -> oneshot::Receiver<Result<MRecv, io::Error>> {
+        let (ret, recv) = oneshot::channel();
+        let job = ReadJob { ret };
+        self.read_job_queue.try_send(job).unwrap();
+        recv
     }
 }
