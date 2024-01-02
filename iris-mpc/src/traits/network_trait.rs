e49ab98f00b53dd0f4c27f92185dc9a31c8e118c
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-01-02 14:36:54 +0100

    feat: add inline attr to trivial async functions in network trait

--- iris-mpc/src/traits/network_trait.rs
+++ iris-mpc/src/traits/network_trait.rs
@@ -25,66 +25,78 @@ pub trait NetworkTrait {
 
     async fn broadcast(&mut self, data: Bytes) -> Result<Vec<BytesMut>, Error>;
 
+    #[inline]
     async fn send_value<R: RingImpl>(&mut self, id: usize, data: R) -> Result<(), Error> {
         let bytes = data.to_bytes();
         self.send(id, bytes).await
     }
 
+    #[inline]
     async fn send_value_next_id<R: RingImpl>(&mut self, data: R) -> Result<(), Error> {
         let bytes = data.to_bytes();
         self.send_next_id(bytes).await
     }
 
+    #[inline]
     async fn send_value_prev_id<R: RingImpl>(&mut self, data: R) -> Result<(), Error> {
         let bytes = data.to_bytes();
         self.send_prev_id(bytes).await
     }
 
+    #[inline]
     async fn receive_value<R: RingImpl>(&mut self, id: usize) -> Result<R, Error> {
         self.receive(id)
             .await
             .map(|bytes| R::from_bytes_mut(bytes).expect("Invalid bytes"))
     }
 
+    #[inline]
     async fn receive_value_prev_id<R: RingImpl>(&mut self) -> Result<R, Error> {
         self.receive_prev_id()
             .await
             .map(|bytes| R::from_bytes_mut(bytes).expect("Invalid bytes"))
     }
 
+    #[inline]
     async fn receive_value_next_id<R: RingImpl>(&mut self) -> Result<R, Error> {
         self.receive_next_id()
             .await
             .map(|bytes| R::from_bytes_mut(bytes).expect("Invalid bytes"))
     }
 
+    #[inline]
     async fn send_vec<R: RingImpl>(&mut self, id: usize, data: Vec<R>) -> Result<(), Error> {
         let bytes = ring_vec_to_bytes(data);
         self.send(id, bytes).await
     }
 
+    #[inline]
     async fn send_vec_next_id<R: RingImpl>(&mut self, data: Vec<R>) -> Result<(), Error> {
         let bytes = ring_vec_to_bytes(data);
         self.send_next_id(bytes).await
     }
 
+    #[inline]
     async fn send_vec_prev_id<R: RingImpl>(&mut self, data: Vec<R>) -> Result<(), Error> {
         let bytes = ring_vec_to_bytes(data);
         self.send_prev_id(bytes).await
     }
 
+    #[inline]
     async fn receive_vec<R: RingImpl>(&mut self, id: usize) -> Result<Vec<R>, Error> {
         self.receive(id)
             .await
             .map(|bytes| ring_vec_from_bytes(bytes).expect("Invalid bytes"))
     }
 
+    #[inline]
     async fn receive_vec_prev_id<R: RingImpl>(&mut self) -> Result<Vec<R>, Error> {
         self.receive_prev_id()
             .await
             .map(|bytes| ring_vec_from_bytes(bytes).expect("Invalid bytes"))
     }
 
+    #[inline]
     async fn receive_vec_next_id<R: RingImpl>(&mut self) -> Result<Vec<R>, Error> {
         self.receive_next_id()
             .await
