ae7aef0a85913124cbdf2c953a371f2c41362f77
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-01-02 13:37:40 +0100

    feat: use new manged channel in Aby3Network

--- iris-mpc/src/aby3/network.rs
+++ iris-mpc/src/aby3/network.rs
@@ -4,17 +4,15 @@ use super::id::PartyID;
 use crate::error::Error;
 use crate::traits::network_trait::NetworkTrait;
 use bytes::{Bytes, BytesMut};
-use futures::{SinkExt, StreamExt};
-use mpc_net::channel::BytesChannel;
+use mpc_net::channel::ChannelHandle;
 use mpc_net::config::NetworkConfig;
 use mpc_net::MpcNetworkHandler;
-use quinn::{RecvStream, SendStream};
 
 pub struct Aby3Network {
     handler: MpcNetworkHandler,
     id: PartyID,
-    channel_send: BytesChannel<RecvStream, SendStream>,
-    channel_recv: BytesChannel<RecvStream, SendStream>,
+    channel_send: ChannelHandle<Bytes, BytesMut>,
+    channel_recv: ChannelHandle<Bytes, BytesMut>,
 }
 
 impl Aby3Network {
@@ -32,6 +30,9 @@ impl Aby3Network {
 
         let channel_send = channels.remove(&next_id).ok_or(Error::ConfigError)?;
         let channel_recv = channels.remove(&prev_id).ok_or(Error::ConfigError)?;
+
+        let channel_send = ChannelHandle::manage(channel_send);
+        let channel_recv = ChannelHandle::manage(channel_recv);
 
         Ok(Self {
             handler,
@@ -58,9 +59,11 @@ impl NetworkTrait for Aby3Network {
     async fn send(&mut self, id: usize, data: Bytes) -> io::Result<()> {
         tracing::trace!("send_id {}->{}: {:?}", self.id, id, data);
         let res = if id == usize::from(self.id.next_id()) {
-            self.channel_send.send(data).await
+            let _ = self.channel_send.send(data).await;
+            Ok(())
         } else if id == usize::from(self.id.prev_id()) {
-            self.channel_recv.send(data).await
+            let _ = self.channel_recv.send(data).await;
+            Ok(())
         } else {
             Err(io::Error::new(io::ErrorKind::Other, "Invalid ID"))
         };
@@ -70,30 +73,30 @@ impl NetworkTrait for Aby3Network {
 
     async fn send_next_id(&mut self, data: Bytes) -> io::Result<()> {
         tracing::trace!("send {}->{}: {:?}", self.id, self.id.next_id(), data);
-        let res = self.channel_send.send(data).await;
+        let _ = self.channel_send.send(data).await;
         tracing::trace!("send {}->{}: done", self.id, self.id.next_id());
-        res
+        Ok(())
     }
 
     async fn send_prev_id(&mut self, data: Bytes) -> io::Result<()> {
         tracing::trace!("send {}->{}: {:?}", self.id, self.id.prev_id(), data);
-        let res = self.channel_recv.send(data).await;
+        let _ = self.channel_recv.send(data).await;
         tracing::trace!("send {}->{}: done", self.id, self.id.prev_id());
-        res
+        Ok(())
     }
 
     async fn receive(&mut self, id: usize) -> Result<BytesMut, io::Error> {
         tracing::trace!("recv_id {}<-{}: ", self.id, id);
         let buf = if id == usize::from(self.id.prev_id()) {
-            self.channel_recv.next().await
+            self.channel_recv.recv().await
         } else if id == usize::from(self.id.next_id()) {
-            self.channel_send.next().await
+            self.channel_send.recv().await
         } else {
             return Err(io::Error::new(io::ErrorKind::Other, "Invalid ID"));
         };
         tracing::trace!("recv_id {}<-{}: done", self.id, id);
 
-        if let Some(maybe_packet) = buf {
+        if let Ok(maybe_packet) = buf {
             maybe_packet
         } else {
             Err(io::Error::new(
@@ -105,9 +108,9 @@ impl NetworkTrait for Aby3Network {
 
     async fn receive_prev_id(&mut self) -> io::Result<BytesMut> {
         tracing::trace!("recv {}<-{}: ", self.id, self.id.prev_id());
-        let buf = self.channel_recv.next().await;
+        let buf = self.channel_recv.recv().await;
         tracing::trace!("recv {}<-{}: done", self.id, self.id.prev_id());
-        if let Some(maybe_packet) = buf {
+        if let Ok(maybe_packet) = buf {
             maybe_packet
         } else {
             Err(io::Error::new(
@@ -119,9 +122,9 @@ impl NetworkTrait for Aby3Network {
 
     async fn receive_next_id(&mut self) -> io::Result<BytesMut> {
         tracing::trace!("recv {}<-{}: ", self.id, self.id.next_id());
-        let buf = self.channel_send.next().await;
+        let buf = self.channel_send.recv().await;
         tracing::trace!("recv {}<-{}: done", self.id, self.id.next_id());
-        if let Some(maybe_packet) = buf {
+        if let Ok(maybe_packet) = buf {
             maybe_packet
         } else {
             Err(io::Error::new(
@@ -149,40 +152,6 @@ impl NetworkTrait for Aby3Network {
     }
 
     async fn shutdown(self) -> io::Result<()> {
-        let (mut send1, mut recv1) = self.channel_send.split();
-        let (mut send2, mut recv2) = self.channel_recv.split();
-        send1.flush().await?;
-        send1.close().await?;
-        send2.flush().await?;
-        send2.close().await?;
-        drop(send1);
-        drop(send2);
-        if let Some(x) = recv1.next().await {
-            match x {
-                Ok(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Unexpected data on read channel when closing connections",
-                    ));
-                }
-                Err(e) => {
-                    return Err(e);
-                }
-            }
-        }
-        if let Some(x) = recv2.next().await {
-            match x {
-                Ok(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Unexpected data on read channel when closing connections",
-                    ));
-                }
-                Err(e) => {
-                    return Err(e);
-                }
-            }
-        }
         Ok(())
     }
 }
