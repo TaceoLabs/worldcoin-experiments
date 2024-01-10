ef1b0a3b6abe359be0c47328ed4a8e0b89fc028c
Roman Walch <walch@taceo.io>
2024-01-10 10:14:23 +0100

    .

--- iris-mpc/src/spdzwise/triples.rs
+++ iris-mpc/src/spdzwise/triples.rs
@@ -74,22 +74,72 @@ impl Triples {
         self.c.extend(other.c);
     }
 
-    pub fn add_t<T: Sharable>(
-        &mut self,
+    fn push_t<T: Sharable>(a: Aby3Share<T>, buffer: &mut Vec<Aby3Share<u128>>) {
+        let (aa, ab) = a.get_ab();
+        let aa = aa.upgrade_to_128();
+        let ab = ab.upgrade_to_128();
+        buffer.push(Aby3Share::new(aa, ab));
+    }
+
+    fn add_all_t<T: Sharable>(a: Aby3Share<T>, buffer: &mut [Aby3Share<u128>]) {
+        let a_ = buffer.last_mut().expect("Is present");
+        let (aa, ab) = a.get_ab();
+        let aa = aa.upgrade_to_128();
+        let ab = ab.upgrade_to_128();
+
+        a_.a <<= T::Share::K as u32;
+        a_.b <<= T::Share::K as u32;
+        a_.a |= aa;
+        a_.b |= ab;
+    }
+
+    fn split_and_add_t<T: Sharable>(
         a: Aby3Share<T>,
-        b: Aby3Share<T>,
-        c: Aby3Share<T>,
-    ) -> Result<(), Error> {
+        buffer: &mut Vec<Aby3Share<u128>>,
+        open: usize,
+    ) {
+        let a_ = buffer.last_mut().expect("Is present");
+        let (aa, ab) = a.get_ab();
+        let mut aa = aa.upgrade_to_128();
+        let mut ab = ab.upgrade_to_128();
+
+        a_.a <<= open as u32;
+        a_.b <<= open as u32;
+        let mask = (1 << open) - 1;
+        a_.a |= aa.to_owned() & mask;
+        a_.b |= ab.to_owned() & mask;
+
+        aa >>= open as u32;
+        ab >>= open as u32;
+        buffer.push(Aby3Share::new(aa, ab));
+    }
+
+    pub fn add_t<T: Sharable>(&mut self, a: Aby3Share<T>, b: Aby3Share<T>, c: Aby3Share<T>) {
         let open = 128 - self.bits_in_last;
         if open == 0 {
             // put all into a new element
-            let (aa, ab) = a.get_ab();
-            let aa = aa.upgrade_to_128();
-            let ab = ab.upgrade_to_128();
-            self.a.push(Aby3Share::new(aa, ab));
-        } else if open > T::Share::K {
-            // TODO wip
+            Self::push_t(a, &mut self.a);
+            Self::push_t(b, &mut self.b);
+            Self::push_t(c, &mut self.c);
+
+            self.bits_in_last = T::Share::K;
+        } else if open >= T::Share::K {
+            Self::add_all_t(a, &mut self.a);
+            Self::add_all_t(b, &mut self.b);
+            Self::add_all_t(c, &mut self.c);
+
+            self.bits_in_last += T::Share::K;
+        } else {
+            // 0 < open < T::Share::K
+            // We have to split
+            let diff = T::Share::K - open;
+            debug_assert!(open < T::Share::K);
+            debug_assert!(open > 0);
+            Self::split_and_add_t(a, &mut self.a, open);
+            Self::split_and_add_t(b, &mut self.b, open);
+            Self::split_and_add_t(c, &mut self.c, open);
+
+            self.bits_in_last = diff;
         }
-        todo!()
     }
 }
